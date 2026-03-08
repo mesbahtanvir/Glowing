8972022a9ed8ca7d9ed416167801b1ee75f5fbee
Mesbah Tanvir <mesbah.tanvir.cs@gmail.com>
2026-03-08 14:52:13 -0400

    Initial Commit

---
+++ Glowing/GlowingApp.swift
@@ -0,0 +1,32 @@
+//
+//  GlowingApp.swift
+//  Glowing
+//
+//  Created by Mesbah Tanvir on 2026-03-08.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GlowingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ GlowingTests/GlowingTests.swift
@@ -0,0 +1,16 @@
+//
+//  GlowingTests.swift
+//  GlowingTests
+//
+//  Created by Mesbah Tanvir on 2026-03-08.
+//
+
+import Testing
+
+struct GlowingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
